96de05a9fe05f80e5120578b76b58b84577e8402
Simon Busch <morphis@gravedo.de>
2013-01-16 07:59:05 +0100

    Implemented service isTelephonyReady API method
    Without having the isTelephonyReady method available luna-systemui will continue to poll
    our service until it gets a response which never happens and will cause the WebAppMgr
    process to consume about 100% of the CPU cycles.
    
    Open-webOS-DCO-1.0-Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/telephonyservice.c
+++ src/telephonyservice.c
@@ -42,6 +42,7 @@ struct telephony_service {
 };
 
 bool _service_subscribe_cb(LSHandle *handle, LSMessage *message, void *user_data);
+bool _service_is_telephony_ready_cb(LSHandle *handle, LSMessage *message, void *user_data);
 bool _service_power_set_cb(LSHandle* lshandle, LSMessage *message, void *user_data);
 bool _service_power_query_cb(LSHandle *lshandle, LSMessage *message, void *user_data);
 bool _service_platform_query_cb(LSHandle *handle, LSMessage *message, void *user_data);
@@ -52,6 +53,7 @@ bool _service_network_status_query_cb(LSHandle *handle, LSMessage *message, void
 
 static LSMethod _telephony_service_methods[]  = {
 	{ "subscribe", _service_subscribe_cb },
+	{ "isTelephonyReady", _service_is_telephony_ready_cb },
 	{ "powerSet", _service_power_set_cb },
 	{ "powerQuery", _service_power_query_cb },
 	{ "platformQuery", _service_platform_query_cb },
@@ -278,6 +280,31 @@ void telephony_service_signal_strength_changed_notify(struct telephony_service *
 	luna_service_post_subscription(service->private_service, "/", "signalStrengthQuery", reply_obj);
 
 	j_release(&reply_obj);
+}
+
+/**
+ * @brief Check wether telephony service is ready
+ **/
+
+bool _service_is_telephony_ready_cb(LSHandle *handle, LSMessage *message, void *user_data)
+{
+	struct telephony_service *service = user_data;
+	jvalue_ref reply_obj = NULL;
+	jvalue_ref extended_obj = NULL;
+
+	reply_obj = jobject_create();
+	extended_obj = jobject_create();
+
+	jobject_put(reply_obj, J_CSTR_TO_JVAL("returnValue"), jboolean_create(true));
+	jobject_put(extended_obj, J_CSTR_TO_JVAL("radioConnected"), jboolean_create(service->initialized));
+	jobject_put(reply_obj, J_CSTR_TO_JVAL("extended"), extended_obj);
+
+	if(!luna_service_message_validate_and_send(handle, message, reply_obj))
+		luna_service_message_reply_error_internal(handle, message);
+
+	j_release(reply_obj);
+
+	return true;
 }
 
 /**
